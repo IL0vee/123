df50f139ec63ffc164a3c1fd27573701c47af506
IL0vee <88659885+IL0vee@users.noreply.github.com>
2026-02-19 12:58:22 +0400

    Add files via upload

---
+++ Лаба2/PhoneCompany/Registration.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace PhoneCompany
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.LoginLabel = new System.Windows.Forms.Label();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.AutorizationButton = new System.Windows.Forms.Button();
+            this.RegistrationButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(713, 12);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 38);
+            this.ExitButton.TabIndex = 1;
+            this.ExitButton.Text = "X";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // LoginLabel
+            // 
+            this.LoginLabel.AutoSize = true;
+            this.LoginLabel.Location = new System.Drawing.Point(325, 192);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(41, 13);
+            this.LoginLabel.TabIndex = 6;
+            this.LoginLabel.Text = "Логин:";
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(328, 208);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(136, 20);
+            this.LoginTextBox.TabIndex = 7;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(325, 231);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(48, 13);
+            this.PasswordLabel.TabIndex = 8;
+            this.PasswordLabel.Text = "Пароль:";
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(328, 247);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(136, 20);
+            this.PasswordTextBox.TabIndex = 9;
+            // 
+            // AutorizationButton
+            // 
+            this.AutorizationButton.Location = new System.Drawing.Point(339, 273);
+            this.AutorizationButton.Name = "AutorizationButton";
+            this.AutorizationButton.Size = new System.Drawing.Size(102, 32);
+            this.AutorizationButton.TabIndex = 10;
+            this.AutorizationButton.Text = "Авторизация";
+            this.AutorizationButton.UseVisualStyleBackColor = true;
+            this.AutorizationButton.Click += new System.EventHandler(this.AutorizationButton_Click);
+            // 
+            // RegistrationButton
+            // 
+            this.RegistrationButton.Location = new System.Drawing.Point(339, 321);
+            this.RegistrationButton.Name = "RegistrationButton";
+            this.RegistrationButton.Size = new System.Drawing.Size(102, 32);
+            this.RegistrationButton.TabIndex = 11;
+            this.RegistrationButton.Text = "Регистрация";
+            this.RegistrationButton.UseVisualStyleBackColor = true;
+            this.RegistrationButton.Click += new System.EventHandler(this.RegistrationButton_Click);
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RegistrationButton);
+            this.Controls.Add(this.AutorizationButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.LoginLabel);
+            this.Controls.Add(this.ExitButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Registration";
+            this.Text = "Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label LoginLabel;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button AutorizationButton;
+        private System.Windows.Forms.Button RegistrationButton;
+    }
+}
